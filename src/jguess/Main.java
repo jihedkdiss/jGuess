f20241e2574ae28953089efe1bc06fc9b24886dc
Jihed Kdiss <jihedkdiss@outlook.com>
2021-07-22 22:28:45 +0100

    :100: Add comments and loop
    Now it should run as intended

--- src/jguess/Main.java
+++ src/jguess/Main.java
@@ -6,22 +6,30 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Object declarations
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
+        // Welcome banner
         System.out.println("== jGuess ==");
         System.out.println("My number is less than 10!");
+
+        // Variables
         int number = random.nextInt(10);
+        int userNumber = 15052004; // That's my birth date!
 
-        System.out.print("My number is: ");
-        int userNumber = input.nextInt();
-
-        if (userNumber < number) {
-            System.out.println("My number is bigger!");
-        } else if (userNumber > number) {
-            System.out.println("My number is smaller!");
-        } else {
-            System.out.println("That's my number!");
+        while(userNumber != number) {
+            System.out.print("My number is: ");
+            userNumber = input.nextInt();
+            if (userNumber < number) {
+                System.out.println("My number is bigger!");
+            } else if (userNumber > number) {
+                System.out.println("My number is smaller!");
+            } else {
+                System.out.println("That's my number!");
+            }
         }
+
     }
 }
